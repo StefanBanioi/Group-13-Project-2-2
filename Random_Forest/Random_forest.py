7ce5092856e111cf6879b86fbd65a76a50427396
StefanBanioi <stefanbanioi3@gmail.com>
2024-05-16 14:08:45 +0200

    New Dataset and cleaning
    Added the new dataset and have changed the way certain collums so that we can use it with our model here this being RF where we can't really use strings so we have to change them to numerical values. This is done by using change all references in the dataset. I changed the hereditary_diseases and the job_title. I would also need to do the cities but these are 91 different ones so it can be a bit annoying and long. i'll do that later but this is so d-far what i did. :D

--- Random_Forest/Random_forest.py
+++ Random_Forest/Random_forest.py
@@ -15,16 +15,91 @@ import pandas as pd
 
 # Load the CSV file into a DataFrame
 mydata = pd.read_csv('Random_forest\Medicalpremium.csv')
+#mydata = pd.read_csv('Random_Forest\healthinsurance2.csv')
 mydata.describe()
+
+#X = mydata.drop(['claim'], axis = 1)
+#X = mydata.drop( ['claim'] , axis = 1)
+#y = mydata['claim']
+#X.shape
+
+# Load the CSV file into a DataFrame
+df = pd.read_csv('Random_Forest\healthinsurance2.csv')
+
+# Get the unique city and count them
+unique_city = df['city'].unique()
+count = len(unique_city)
+
+print(f"There are {count} unique cities in the dataset")
 
 
 # In[14]:
 
 
 # The input features and output features
+ 
 X = mydata.drop(['PremiumPrice'], axis = 1)
 y = mydata['PremiumPrice']
 X.shape
+
+
+# DO NOT delete this lines of comments
+'''
+I have changed the column of sex and changed all the male to 1 and the female to 0. I did this by changing all acourances 
+I have changed the collom hereditary_diseases as follow: 
+* NoDisease = 0
+* Arthritis = 1
+* Alzheimer = 2
+* Diabetes = 3
+* Cancer = 4
+* EyeDisease = 5
+* Obesity = 6
+* High BP = 7
+* HeartDisease = 8
+* Epilepsy = 9
+
+
+I have changed the collom job_title as follow:
+* Student = 0
+* Actor = 1
+* Academician = 2
+* Engineer = 3
+* Chef = 4
+* HomeMakers = 5
+* Dancer = 6
+* DataScientist = 7
+* Singer = 8
+* Doctor = 9
+* Manager = 10
+* Photographer = 11
+* CA = 12
+* Beautician = 13
+* Accountant = 14
+* CEO = 15
+* HouseKeeper = 16
+* Police = 17
+* Lawyer = 18
+* FilmMaker = 19
+* Politician = 20
+* ITProfessional = 21
+* Architect = 22
+* Technician = 23
+* FashionDesigner = 24
+* Buisnessman = 25
+* GovEmployee = 26
+* FilmDirector = 27
+* Farmer = 28
+* Journalist = 29
+* Analyst = 30
+* Blogger = 31
+* Labourer = 32
+* DefencePersonnels = 33
+* Clerks = 34
+'''
+
+
+
+
 
 
 # In[15]:
